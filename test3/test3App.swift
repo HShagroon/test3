5ce9d493136658c04ee37bb8883fc3aa7fa1a180
HShagroon <haneenshagroon@gmail.com>
2023-10-05 12:53:27 +0300

    Initial Commit

---
+++ test3/test3App.swift
@@ -0,0 +1,17 @@
+//
+//  test3App.swift
+//  test3
+//
+//  Created by Haneen Rida Shagroon on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
